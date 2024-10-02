215f3774ba07744d9d40c73785823de70cf5e274
Ken Richlin <krichlin@gmail.com>
2024-10-02 19:17:05 -0400

    added python-dotenv to requirements.txt

--- server/config.py
+++ server/config.py
@@ -3,16 +3,16 @@
 # Standard library imports
 
 # Remote library imports
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-
 from sqlalchemy import MetaData
 from dotenv import load_dotenv
-import os
+
 
 # Local imports
 
